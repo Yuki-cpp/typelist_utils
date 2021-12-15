8c28db5c75f8e545311f5eb5a0fef22bc9bfebad
Leo Ghafari <leo@ascent.ai>
2021-12-15 18:04:40 +0900

    test build with conan

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 
 class TypelistUtilsConan(ConanFile):
     name = "typelist_utils"
-    version = "0.0.1"
+    version = "0.0.2"
 
     # Optional metadata
     license = "MIT"
