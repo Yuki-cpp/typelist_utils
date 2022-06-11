c89bb6b40ccf7ec8c406907005f5dfef05905870
Leo <leo@ascent.ai>
2022-06-11 19:11:18 +0900

    Switch to vckpg for dependencies

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN
 
 
-#include <catch2/catch.hpp>
-#include <catch2/catch_reporter_sonarqube.hpp>
+#include <catch.hpp>
+#include <catch2/reporters/catch_reporter_sonarqube.hpp>
